e35543f77ebda053ed3f0068c482b22487a6f936
Hadrien Froger <hadrien@octree.ch>
2026-06-15 08:17:49 +0100

    release(0.1.5): enhance decidim:update

--- lib/decidim/custom_user_fields/version.rb
+++ lib/decidim/custom_user_fields/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   module CustomUserFields
     def self.version
-      "0.1.4" # DO NOT UPDATE MANUALLY
+      "0.1.5" # DO NOT UPDATE MANUALLY
     end
 
     def self.decidim_version
